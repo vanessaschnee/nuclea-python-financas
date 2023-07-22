0c038015492efdc587f64517051f2ebccf67f58f
Vanessa <vanessa.omasch@gmail.com>
2023-07-21 21:42:53 -0300

    menu

---
+++ main.py
@@ -0,0 +1,51 @@
+control = True
+dic_client = {}
+list_client = []
+
+while control:
+    menu = int(input("Seja bem vindo(a) ao sistema de gerenciamento de carteira de ações da Nuclea. "
+                     "Selecione uma as opções abaixo: \n "
+                     "1 - Cadastrar Cliente\n "
+                     "2 - Cadastrar ação\n "
+                     "3 = Realizar análise da carteira\n "
+                     "4 - Imprimir relatório da carteira\n "
+                     "5 - Sair\n "
+                     "Digite a opção desejada: "))
+
+    if menu == 1:
+        print("informe os dados do cliente:")
+        nome = input("Nome: ")
+        cpf = input("CPF: ")
+        rg = input("RG: ")
+        data_nascimento = input("Data de nascimento: ")
+        cep = input("CEP: ")
+        num_casa = int(input("Número casa: "))
+
+        dic_client = {'nome': nome,
+                      'cpf': cpf,
+                      'rg': rg,
+                      'data_nascimento': data_nascimento,
+                      'cep': cep,
+                      'numero_casa': num_casa}
+
+        list_client.append(dic_client)
+        print(list_client)
+
+        return_menu = input("Deseja retornar ao menu principal [sim/nao]? ")
+
+        if return_menu == 'sim':
+            control = True
+        else:
+            print("Obrigada por utilizar nosso programa.")
+            control = False
+
+    elif menu == '2':
+        pass
+    elif menu == '3':
+        pass
+    elif menu == '4':
+        pass
+    elif menu == '5':
+        print("Obrigada por utilizar nosso programa.")
+    else:
+        print("Opção inválida, tente novamente.")
