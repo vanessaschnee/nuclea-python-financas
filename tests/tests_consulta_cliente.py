64b1cc872fd6fd39fd6fd3a4ff335c5cd8e30851
Vanessa <vanessa.omasch@gmail.com>
2023-08-06 21:34:06 -0300

    Teste de consulta ao cliente

--- tests/tests_consulta_cliente.py
+++ tests/tests_consulta_cliente.py
@@ -1,14 +1,31 @@
 import unittest
 from unittest.mock import patch
+from io import StringIO
 from main import main
 
-
-class TestStringMethods(unittest.TestCase):
+class TestMainFunction(unittest.TestCase):
     def test_consulta_cliente(self):
 
-        inputs = [1, 2, "123.456.789-01", 5, 'nao']
+        inputs = [1, 2, "987.654.321-09", 5, 'nao']
 
-        with patch("builtins.input", side_effect=inputs):
+        with patch("builtins.input", side_effect=inputs), \
+             patch("sys.stdout", new=StringIO()) as output:
             main()
 
-        self.assertIn()
+        resultado_saida = output.getvalue()
+
+        self.assertIn("Dados do cliente-", resultado_saida)
+        self.assertIn("Nome: Maria Oliveira", resultado_saida)
+        self.assertIn("CPF: 987.654.321-09", resultado_saida)
+        self.assertIn("RG: 123456789", resultado_saida)
+        self.assertIn("Data de Nascimento: 1982-02-28", resultado_saida)
+        self.assertIn("Logradouro: Av. das Palmeiras, 456", resultado_saida)
+        self.assertIn("Bairro: Jardim Botânico", resultado_saida)
+        self.assertIn("Cidade: Rio de Janeiro", resultado_saida)
+        self.assertIn("Estado: RJ", resultado_saida)
+        self.assertIn("Num_casa: 456", resultado_saida)
+        self.assertIn("CEP: 56789-321", resultado_saida)
+
+if __name__ == '__main__':
+    unittest.main()
+
